0b8e90e99d84ea563cfd31e059f47aa33b8a5f1e
unknown <williamgtrigg@gmail.com>
2022-04-13 00:20:08 -0400

    updating parsing functionality

--- src/frontend/src/components/ParseExcel.jsx
+++ src/frontend/src/components/ParseExcel.jsx
@@ -8,17 +8,30 @@ export const ParseExcel = () => {
         const file = e.target.files[0];
         setFileName(file.name);
         const data = await file.arrayBuffer();
-        const workbook = readFile(data, { sheetRows: 3});
+        const workbook = readFile(data);
 
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+        const worksheet1 = workbook.Sheets[workbook.SheetNames[0]];
+        var range = utils.decode_range(worksheet1['!ref']);
+        var numStudents = range.e.r - range.s.r - 2;
+        //console.log(numStudents);
 
         //var range = J.utils.decode_range(worksheet['!ref']);
         //range.s.r += 2;
         //worksheet['!ref'] = J.utils.encode_range(range);
 
-        const jsonData = utils.sheet_to_json(worksheet);
-
+        const worksheet2 = workbook.Sheets[workbook.SheetNames[1]];
+        const jsonData = utils.sheet_to_json(worksheet2, { header: 1, defval: "" });
         console.log(jsonData);
+        var col1 = jsonData.map(function(value,index) { return value[0]; });
+        var lb_start = col1.lastIndexOf("Position");
+
+        for(var i = 1; i <= numStudents; i++)
+        {
+            console.log(jsonData[0][lb_start + i] + jsonData[1][lb_start + i] + jsonData[2][lb_start + i]);
+        }
+
+        //console.log(jsonData[lb_start + 1][1]);
+        console.log(lb_start);
     };
     
     return (
