c90491249b7efd04075a498238b123affc225736
unknown <williamgtrigg@gmail.com>
2022-04-16 17:09:32 -0400

    initial update database functionality

--- src/frontend/src/components/import-excel.jsx
+++ src/frontend/src/components/import-excel.jsx
@@ -1,17 +1,17 @@
-import React, { Component, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { readFile, utils } from 'xlsx';
+
 import UserDataService from "../services/user";
+import User from './user';
+
+//for debuggin
+//import http from "../http-common";
 
 export const ImportExcel = () => {
     const [file, setFile] = useState(null);
     const [courseName, setCourseName] = useState(null);
-    //const [studentsInClass, setStudentsInClass] = useState([null]);
     const [studentName, setStudentName] = useState(null);
-    const [student, setStudent] = useState(null);
-
-    useEffect( () => {
-        retrieveStudent(studentName);
-    }, [studentName]);
+    const [students, setStudents] = useState([]);
 
     // function that updates the excel file that is inputted into form
     const handleFileChange = async (e) => {
@@ -25,32 +25,9 @@ export const ImportExcel = () => {
         setCourseName(courseName);
     };
 
-    // 
-    //const retrieveClass = () => {
-    //    UserDataService.getClass(courseName)
-    //      .then((response) => {
-    //        console.log(response.data);
-    //        setStudentsInClass(response.data.users);
-    //      })
-    //      .catch((e) => {
-    //        console.log(e);
-    //      });
-    //};
-
-    //
-    const retrieveStudent = (name) => {
-        UserDataService.getUser(name)
-          .then((response) => {
-              console.log(response.data);
-              setStudent(response.data);
-          })
-          .catch((e) => {
-              console.log(e);
-          });
-    }; 
-    
     // function that submits new quiz data to database
     const handleSubmit = async (e) => {
+        e.preventDefault();
         if(file == null)
         {
             return;
@@ -70,16 +47,87 @@ export const ImportExcel = () => {
         var lb_start = col.lastIndexOf("Position");
 
         // loops through every student in leaderboard,
+        //var lb = Array.from(Array(numStudents), () => new Array(3));
         for(var i = 1; i <= numStudents; i++)
         {
             console.log(jsonData[lb_start + i][0] + " " + jsonData[lb_start + i][1] +  " " + jsonData[lb_start + i][3]);
+            //lb[i - 1][0] = jsonData[lb_start + i][0];
+            //lb[i - 1][1] = jsonData[lb_start + i][1];
+            //lb[i - 1][2] = jsonData[lb_start + i][2];
+            const response = await  UserDataService.getUser(jsonData[lb_start + i][1]);
+            console.log(response.data)
+            if(response.data.total_results != 0) // user already exists in DB
+            {
+                var username = response.data.users[0].username;
+                //console.log(username);
+                var courses = response.data.users[0].classes;
+                var found = false;
+                var index = -1;
+                for(let i = 0; i < courses.length; i++)
+                {
+                    if(courses[i] == courseName)
+                    {
+                        found = true;
+                        index = i;
+                        break;
+                    }
+                }       
+                if(found) // user already has a score for specified course
+                {
+                    console.log(username + " is in course " + courseName);
+                    response.data.users[0].scores[index] += jsonData[lb_start + i][3];
+                    console.log(response.data.users[0].scores[index]);
+                    
+                    const result = await UserDataService.updateUser(response.data); 
+                    console.log(result.data);
+                }
+                else // user does not yet have a score for specified course
+                {
+                    console.log(username + " is not in course " + courseName);
+                }
+            }
+            else // user does exist in DB
+            {
+                console.log("Not found in DB");
+            }
+            //UserDataService.getUser(jsonData[lb_start + i][1])
+            //.then((response) => {
+                //handleStudent(response, i, lb_start, jsonData);
+                //console.log(response.data)
+                //if(response.data.total_results != 0) // user already exists in DB
+                //{
+                //    var username = response.data.users[0].username;
+                //    //console.log(username);
+                //    var courses = response.data.users[0].classes;
+                //    var found = false;
+                //    var index = -1;
+                //    for(let i = 0; i < courses.length; i++)
+                //    {
+                //        if(courses[i] == courseName)
+                //        {
+                //            found = true;
+                //            index = i;
+                //            break;
+                //        }
+                //    }       
+                //   if(found) // user already has a score for specified course
+                //    {
+                //        console.log(username + " is in course " + courseName);
+                //        response.data.users[0].scores[index] += jsonData[lb_start + i][3];
+                //        console.log(response.data.users[0].scores[index]);
+                //    }
+                //    else // user does not yet have a score for specified course
+                //    {
+                //        console.log(username + " is not in course " + courseName);
+                //    }
+                //}
+                //else // user does exist in DB
+                //{
+                //    console.log("Not found in DB");
+                //}
+            //}); 
         }
-        //console.log("students in course: " + courseName);
-        setStudentName("First Post");
-        console.log(student);
-
-        //let student = await UserDataService.getUser("First Post");
-        //console.log(student);
+        //setLeaderboard(lb);
     };
 
     return (
